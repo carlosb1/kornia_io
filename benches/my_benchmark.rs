ac7407dda12ecaaed045b081c43120b9afdf0d51
carlosb1 <mcflurry0@gmail.com>
2022-01-17 00:30:22 +0100

    added flamegraph inside criterion

--- benches/my_benchmark.rs
+++ benches/my_benchmark.rs
@@ -2,6 +2,7 @@ use criterion::{black_box, criterion_group, criterion_main, Criterion};
 use kornia_rs::read_image;
 use std::path::PathBuf;
 
+mod perf;
 fn criterion_benchmark(c: &mut Criterion) {
     let PATH: PathBuf = [env!("CARGO_MANIFEST_DIR"), "clients", "test.jpg"]
         .iter()
@@ -14,6 +15,12 @@ fn criterion_benchmark(c: &mut Criterion) {
         })
     });
 }
-
+/*
 criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
+*/
+criterion_group! {name = benches;
+    config = Criterion::default().with_profiler(perf::FlamegraphProfiler::new(100));
+    targets = criterion_benchmark
+}
 criterion_main!(benches);
